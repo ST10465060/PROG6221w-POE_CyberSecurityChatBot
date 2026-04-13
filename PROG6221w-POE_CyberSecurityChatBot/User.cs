4cb003e0de049d55483743282af2f7b6e2164691
ST10465060 <st10465060@rcconnect.edu.za>
2026-04-13 17:23:10 +0200

    Added User class with automatic properties and AudioPlayer class

---
+++ PROG6221w-POE_CyberSecurityChatBot/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221w_POE_CyberSecurityChatBot
+{
+    internal class User
+    {
+    }
+}
