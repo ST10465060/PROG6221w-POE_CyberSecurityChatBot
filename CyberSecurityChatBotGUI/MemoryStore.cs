ca3aea0726ce02f8ed3f18ed7a2f6ad10c7b384d
ST10465060 <st10465060@rcconnect.edu.za>
2026-05-28 11:43:23 +0200

    Add sentiment detection and user memory

--- CyberSecurityChatBotGUI/MemoryStore.cs
+++ CyberSecurityChatBotGUI/MemoryStore.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CyberSecurityChatBotGUI
 {
-    internal class MemoryStore
+    // stores things the user tells us so we can recall them later
+    class MemoryStore
     {
+        // automatic properties for the two main things we track
+        public string UserName { get; set; }
+        public string FavouriteTopic { get; set; }
+
+        // a dictionary for any extra info we might want to store
+        private Dictionary<string, string> _extraMemory;
+
+        public MemoryStore()
+        {
+            UserName = "Friend";
+            FavouriteTopic = "";
+            _extraMemory = new Dictionary<string, string>();
+        }
+
+        // saves a key-value pair into memory
+        public void Store(string key, string value)
+        {
+            _extraMemory[key] = value;
+        }
+
+        // retrieves a value by its key, returns empty string if not found
+        public string Recall(string key)
+        {
+            if (_extraMemory.ContainsKey(key))
+            {
+                return _extraMemory[key];
+            }
+            return "";
+        }
+
+        // builds a personalised sentence if we know the user's favourite topic
+        public string GetPersonalisedOpener()
+        {
+            if (!string.IsNullOrEmpty(FavouriteTopic))
+            {
+                return $"As someone interested in {FavouriteTopic}, you might find this useful. ";
+            }
+            return "";
+        }
     }
 }
--- CyberSecurityChatBotGUI/SentimentDetector.cs
+++ CyberSecurityChatBotGUI/SentimentDetector.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CyberSecurityChatBotGUI
 {
-    internal class SentimentDetector
+    // the different moods we can detect
+    enum Sentiment { Neutral, Worried, Curious, Frustrated, Happy }
+
+    class SentimentDetector
     {
+        // maps each sentiment to a list of words that indicate that mood
+        private Dictionary<Sentiment, List<string>> _triggerWords;
+
+        public SentimentDetector()
+        {
+            _triggerWords = new Dictionary<Sentiment, List<string>>
+            {
+                { Sentiment.Worried, new List<string> { "worried", "scared", "afraid", "anxious", "nervous", "unsafe", "concern" } },
+                { Sentiment.Curious, new List<string> { "curious", "wondering", "interested", "want to know", "how does", "tell me about" } },
+                { Sentiment.Frustrated, new List<string> { "frustrated", "annoyed", "confused", "don't understand", "stuck", "hate" } },
+                { Sentiment.Happy, new List<string> { "great", "thanks", "helpful", "awesome", "love it", "amazing", "thank you" } }
+            };
+        }
+
+        // checks the user's message for any mood trigger words
+        public Sentiment Detect(string input)
+        {
+            string lower = input.ToLower();
+
+            foreach (var pair in _triggerWords)
+            {
+                foreach (string trigger in pair.Value)
+                {
+                    if (lower.Contains(trigger))
+                    {
+                        return pair.Key;
+                    }
+                }
+            }
+
+            return Sentiment.Neutral;
+        }
+
+        // returns an empathetic response based on the detected mood
+        public string GetSentimentResponse(Sentiment mood)
+        {
+            switch (mood)
+            {
+                case Sentiment.Worried:
+                    return "It's completely understandable to feel that way. Let me help ease your concerns. ";
+                case Sentiment.Curious:
+                    return "I love your curiosity! Let me share what I know. ";
+                case Sentiment.Frustrated:
+                    return "I get that it can be frustrating. Let me try to explain it more clearly. ";
+                case Sentiment.Happy:
+                    return "Glad to hear that! Always happy to help. ";
+                default:
+                    return "";
+            }
+        }
     }
 }
